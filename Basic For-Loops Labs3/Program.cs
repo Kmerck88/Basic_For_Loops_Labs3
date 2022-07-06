211606496b2a7888afcf986393759bdfaa3fda7f
kmerck88 <Kevin.Merck88@gmail.com>
2022-07-06 17:23:25 -0400

    first commit

---
+++ Basic For-Loops Labs3/Program.cs
@@ -0,0 +1,26 @@
+﻿int i, n, sum = 0;
+
+Console.WriteLine("Enter a number:");
+
+n = Convert.ToInt32(Console.ReadLine());
+
+
+for (i = 1; i <= n; i++) {
+    sum += i;
+
+}
+    Console.WriteLine("Total sum: " + sum);
+
+ Console.WriteLine("Would you like to continue (y/n)? ");
+if (Console.ReadLine() == "n")
+{
+  
+Console.WriteLine("Goodbye!");
+}
+
+
+
+
+
+
+
